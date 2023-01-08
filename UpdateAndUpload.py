34a8725d26a9effbf7a0c04dd5d51c3210b3a1ab
yzh <yzh@ivu.de>
2023-01-08 03:02:25 +0100

    commit at 2023-01-08_03:02:23

--- UpdateAndUpload.py
+++ UpdateAndUpload.py
@@ -273,6 +273,7 @@ def main():
     gitRepoSynchron(r'd:\File_Storage\File_Markdown\HTML5_CSS3_HeiMaPink_YZHNote')
     gitRepoSynchron(r'd:\File_Storage\File_Markdown\JavaSE_HanShunPing_YZHNote')
     gitRepoSynchron(r'd:\File_Storage\File_Markdown\Javascript_HeiMaPink_YZHNote')
+    gitRepoSynchron(r'd:\File_Storage\File_Markdown\ComputerNetWork_PackageAnalyse')
 
 
 if __name__ == '__main__':
